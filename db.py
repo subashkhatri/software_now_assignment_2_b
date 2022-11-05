0bbe3901a032073cc315803708e1e1fa0a63aed2
subasskhatrii <subash@teksewa.com>
2022-11-05 17:58:34 +0930

    - Add methods on database for adding, fetching removing and updating wish list application records

--- db.py
+++ db.py
@@ -7,3 +7,25 @@ class Database:
         self.cur.execute(
             "CREATE TABLE IF NOT EXISTS wish_list_items (id INTEGER PRIMARY KEY, list_name text, brand_name text, retailer text, price text)")
         self.db_connection.commit()
+
+    def fetch(self):
+        self.cur.execute("SELECT * FROM parts")
+        rows = self.cur.fetchall()
+        return rows
+
+        def insert(self, part, customer, retailer, price):
+            self.cur.execute("INSERT INTO parts VALUES (NULL, ?, ?, ?, ?)",
+                            (part, customer, retailer, price))
+            self.db_connection.commit()
+
+        def remove(self, id):
+            self.cur.execute("DELETE FROM parts WHERE id=?", (id,))
+            self.db_connection.commit()
+
+        def update(self, id, part, customer, retailer, price):
+            self.cur.execute("UPDATE parts SET part = ?, customer = ?, retailer = ?, price = ? WHERE id = ?",
+                            (part, customer, retailer, price, id))
+            self.db_connection.commit()
+
+        def __del__(self):
+            self.db_connection.close()
