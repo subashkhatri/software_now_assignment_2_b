ca451146adb4ab5fbae56d4ce77e26b38833cff9
subasskhatrii <subash@teksewa.com>
2022-11-05 16:38:53 +0930

    - Add variables in constructor

--- wish_list_controller.py
+++ wish_list_controller.py
@@ -12,6 +12,15 @@ class Application(tk.Frame):
     # do something
     def __init__(self, master):
         self.master = master
+        master.title('Wish list Manager')
+        # width and height
+        master.geometry('700x350')
+        # create widgets/grid
+        self.create_widgets() # todo: create widgets method
+        # Init selected item variable
+        self.selected_item = 0
+        # Populate initial list
+        self.populate_list() # todo: create populate_list method
 
 
 
