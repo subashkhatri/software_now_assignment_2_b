6d47d66d48d659fac33fb7066d323a6cd230b52f
subasskhatrii <subash@teksewa.com>
2022-11-15 21:08:25 +0930

    - Add script for creating database if it does exists

--- db.py
+++ db.py
@@ -1,11 +1,15 @@
 import sqlite3
+import os
 
 class Database:
     def __init__(self, db):
+        if not os.path.exists('store.db'):
+            conn = sqlite3.connect('store.db')
+
         self.db_connection = sqlite3.connect(db)
         self.cur = self.db_connection.cursor()
         self.cur.execute(
-            "CREATE TABLE IF NOT EXISTS wish_list_items (id INTEGER PRIMARY KEY, list_name text, brand_name text, retailer text, price text)")
+            "CREATE TABLE IF NOT EXISTS wish_list_items (id INTEGER PRIMARY KEY, list_name text NOT NULL UNIQUE, brand_name text, retailer text, price text)")
         self.db_connection.commit()
 
     def fetch(self):
